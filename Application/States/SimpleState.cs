efe1c314ae0bc0fe0f3017d4ce4ecd81c308195f
Tobi <buecktobias@gmail.com>
2022-03-20 23:23:15 +0100

    Added Simple State and Simple Transition

---
+++ Application/States/SimpleState.cs
@@ -0,0 +1,49 @@
+﻿using Application.Transitions;
+
+namespace Application.States;
+
+public class SimpleState : AbstractState
+{
+    public string IntroOutput { get; set; }
+    public string OutroOutput { get; set; }
+    public bool EndState { get; set; }
+    
+    
+    public SimpleState(
+        string introOutput ="",
+        List<ITransition>? transitions = null,
+        string outroOutput = "",
+        bool isEndState = false,
+        Func<IGameInformation, IGameInformation>? executeFunc = null)
+    {
+        IntroOutput = introOutput;
+        OutroOutput = outroOutput;
+        EndState = isEndState;
+
+    }
+    
+    public override string GetIntroOutput()
+    {
+        return base.GetIntroOutput();
+    }
+
+    public override string GetOutroOutput()
+    {
+        return base.GetOutroOutput();
+    }
+
+    public override bool IsEndState()
+    {
+        return base.IsEndState();
+    }
+
+    public override IGameInformation Execute(IGameInformation gameInformation)
+    {
+        return base.Execute(gameInformation);
+    }
+
+    protected override void CreateTransitions()
+    {
+        base.CreateTransitions();
+    }
+}
---
+++ Application/Transitions/SimpleTransition.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Transitions;
+
+public class SimpleTransition : AbstractTransition
+{
+    private Func<string, IGameInformation, bool> MatchFunc { get; set; }
+    private IState State { get; set; }
+    public SimpleTransition(Func<string, IGameInformation, bool> matchFunc, IState targetState)
+    {
+        MatchFunc = matchFunc;
+        State = targetState;
+    }
+    public override bool Matches(string input, IGameInformation gameInformation)
+    {
+        return MatchFunc(input, gameInformation);
+    }
+
+    public override IState GetTargetState()
+    {
+        return State;
+    }
+}
