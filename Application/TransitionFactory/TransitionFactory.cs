abd24779c0f7763c844a11d2ea88b906e90a2e74
Tobi <buecktobias@gmail.com>
2022-03-21 11:51:39 +0100

    simplified transitions!

--- Application/TransitionFactory/TransitionFactory.cs
+++ Application/TransitionFactory/TransitionFactory.cs
@@ -12,46 +12,90 @@ public class TransitionFactory : ITransitionFactory
     private TransitionFactory()
     {
         _stateFactory = StateFactory.StateFactory.GetInstance();
-        GameWonTransition = new GameWonTransition();
-        GameDrawTransition = new GameDrawTransition();
-        GameLostTransition = new GameLostTransition();
-        GamePlayTranition = new RockPaperScissorGamePlayTransition();
     }
 
     private ITransition? GameWonTransition { get; set; }
     private ITransition? GameDrawTransition { get; set; }
     private ITransition? GameLostTransition { get; set; }
-    private ITransition? GamePlayTranition { get; set; }
+    private ITransition? GamePlayTransition { get; set; }
     private ITransition? QuitTransition { get; set; }
 
     public ITransition GetNoMatchTransition()
     {
-        if (this.QuitTransition == null)
-        {
-            QuitTransition = new SimpleTransition(((s, information) => s== "quit" ), _stateFactory.GetQuitState());
-        }
         return GetQuitTransition();
     }
 
 
     public ITransition GetGameWonTransition()
     {
+        if (this.GameWonTransition == null)
+        {
+            Func<string, IGameInformation, bool> matchFunc = (s, gameInformation) =>
+            {
+                var wonWithPaper = gameInformation.PlayerInformation == "Papier" &&
+                                   gameInformation.OpponentInformation == "Stein";
+                var wonWithStone = gameInformation.PlayerInformation == "Stein" &&
+                                   gameInformation.OpponentInformation == "Schere";
+                var wonWithScissors = gameInformation.PlayerInformation == "Schere" &&
+                                      gameInformation.OpponentInformation == "Papier";
+                return wonWithPaper || wonWithStone || wonWithScissors;
+            };
+            this.GameWonTransition = new SimpleTransition(matchFunc, _stateFactory.GetQuitState(), "Yeah Sie haben gewonnen!");
+        }
         return GameWonTransition;
     }
 
     public ITransition GetGameDrawTransition()
     {
+        if (this.GameDrawTransition == null)
+        {
+            Func<string, IGameInformation, bool> matchFunc = (s, information) =>
+            {
+                return information.OpponentInformation == information.PlayerInformation;
+            };
+            this.GameDrawTransition = new SimpleTransition(
+                matchFunc, _stateFactory.GetGameStartState(),"Unentschieden ! Nächste Runde !");
+        }
         return GameDrawTransition;
     }
 
     public ITransition GetGameLostTransition()
     {
+        if (this.GameLostTransition == null)
+        {
+            Func<string, IGameInformation, bool> matchFunc = (s, gameInformation) =>
+            {
+                var wonWithPaper = gameInformation.PlayerInformation == "Papier" &&
+                                   gameInformation.OpponentInformation == "Stein";
+                var wonWithStone = gameInformation.PlayerInformation == "Stein" &&
+                                   gameInformation.OpponentInformation == "Schere";
+                var wonWithScissors = gameInformation.PlayerInformation == "Schere" &&
+                                      gameInformation.OpponentInformation == "Papier";
+                var isDraw = gameInformation.PlayerInformation == gameInformation.OpponentInformation;
+                return !wonWithPaper && !wonWithStone && !wonWithScissors &&
+                       !isDraw;
+            };
+            this.GameLostTransition = new SimpleTransition(matchFunc, _stateFactory.GetQuitState(), "Sie haben leider verloren!");
+        }
         return GameLostTransition;
     }
 
     public ITransition GetGamePlayTransition()
     {
-        return GamePlayTranition;
+        if (this.GamePlayTransition == null)
+        {
+            this.GamePlayTransition = new SimpleTransition(
+                ((s, information) => true),
+                _stateFactory.GetOpponentsTurnState(),
+                ((s, information) =>
+                {
+                    information.PlayerInformation = s;
+                    return information;
+                })
+                );
+        }
+        return GamePlayTransition;
+        
     }
 
     public ITransition GetTransition(string identifier)
@@ -68,6 +112,10 @@ public class TransitionFactory : ITransitionFactory
 
     public ITransition GetQuitTransition()
     {
+        if (this.QuitTransition == null)
+        {
+            QuitTransition = new SimpleTransition(((s, information) => s== "quit" ), _stateFactory.GetQuitState());
+        }
         return QuitTransition;
     }
 }
--- Application/Transitions/SimpleTransition.cs
+++ Application/Transitions/SimpleTransition.cs
@@ -3,11 +3,29 @@
 public class SimpleTransition : AbstractTransition
 {
     private Func<string, IGameInformation, bool> MatchFunc { get; set; }
+    private Func<string, IGameInformation, IGameInformation> ExecuteFunc { get; set; }
     private IState State { get; set; }
-    public SimpleTransition(Func<string, IGameInformation, bool> matchFunc, IState targetState)
+    private string Output { get; set; }
+    public SimpleTransition(Func<string, IGameInformation, bool> matchFunc, IState targetState):
+        this(matchFunc, targetState, ((s, information) => information), "")
     {
+    }
+    public SimpleTransition(Func<string, IGameInformation, bool> matchFunc, IState targetState, string output):
+        this(matchFunc, targetState, ((s, information) => information), output)
+    {
+    }
+    public SimpleTransition(Func<string, IGameInformation, bool> matchFunc, IState targetState,Func<string, IGameInformation, IGameInformation> executeFunc ):
+        this(matchFunc, targetState, executeFunc, "")
+    {
+    }
+    
+    public SimpleTransition(Func<string, IGameInformation, bool> matchFunc, IState targetState, Func<string, IGameInformation, IGameInformation> executeFunc, string output)
+    {
+        
         MatchFunc = matchFunc;
         State = targetState;
+        ExecuteFunc = executeFunc;
+        Output = output;
     }
     public override bool Matches(string input, IGameInformation gameInformation)
     {
@@ -17,5 +35,15 @@ public class SimpleTransition : AbstractTransition
     public override IState GetTargetState()
     {
         return State;
+    }
+
+    public override IGameInformation Execute(string input, IGameInformation gameInformation)
+    {
+        return this.ExecuteFunc(input, gameInformation);
+    }
+
+    public override string GetOutput()
+    {
+        return this.Output;
     }
 }
